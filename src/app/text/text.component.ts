df5ee65ce59cb28d32ac8cbc17dbed0b2d1c2fca
Henry Zhang <hzhang@freelancer.com>
2019-06-13 19:19:57 +1000

    Undo experimental changes to text

--- src/app/text/text.component.ts
+++ src/app/text/text.component.ts
@@ -3,7 +3,19 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: `app-text`,
   template: `
-    <ng-content></ng-content>
+    <ng-container [ngSwitch]="style">
+      <p *ngSwitchCase="'normal'">
+        <ng-container *ngTemplateOutlet="injectedContent"></ng-container>
+      </p>
+
+      <strong *ngSwitchCase="'strong'">
+        <ng-container *ngTemplateOutlet="injectedContent"></ng-container>
+      </strong>
+    </ng-container>
+
+    <ng-template #injectedContent>
+      <ng-content></ng-content>
+    </ng-template>
   `
 })
 export class TextComponent {
